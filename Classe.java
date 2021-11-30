57799c50eb2644969c5af317fbf47c5cac124108
WilliamAmorim <40150519+WilliamAmorim@users.noreply.github.com>
2021-11-30 19:10:34 -0300

    Update Classe.java

--- Classe.java
+++ Classe.java
@@ -1,4 +1,10 @@
+package regrasNegocio;
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class Classe {
+    DecimalFormat formatador = new DecimalFormat("0.00");
     int n;
     String[] produtos;
     double[] valores;
@@ -11,6 +17,59 @@ public class Classe {
         this.vendas = new int[12][n];
     }
 
+    public void showMenu(){
+        try {
+            String leftAlignFormat = "| %-15s | %-15s |%n";
+
+            System.out.format("+-----------------+-----------------+%n");
+            System.out.format("| Opção           | Escolha         |%n");
+            System.out.format("+-----------------+-----------------+%n");
+            System.out.format(leftAlignFormat, "Novo Produto", "01");
+            System.out.format(leftAlignFormat, "Adicionar Venda", "02");
+            System.out.format(leftAlignFormat, "Numero Vendas", "03");
+            System.out.format(leftAlignFormat, "Valor Vendas", "04");
+            System.out.format(leftAlignFormat, "Media Anual", "05");
+            System.out.format(leftAlignFormat, "Maior Venda", "06");
+            System.out.format(leftAlignFormat, "Sair", "07");
+            System.out.format("+-----------------+-----------------+%n");
+
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção"));
+            switch (opcao) {
+                case 01:
+                    String nome = JOptionPane.showInputDialog(null, "Nome Produto");
+                    double valor = Double.parseDouble(JOptionPane.showInputDialog(null, "Valor Produto"));
+                    setProduto(nome, valor);
+                    showMenu();
+                    break;
+                case 02:
+                    int mes = Integer.parseInt(JOptionPane.showInputDialog(null, "Mês"));
+                    getAll();
+                    int index = Integer.parseInt(JOptionPane.showInputDialog(null, "Index do Produto"));
+                    int qtd = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero de vendas"));
+                    setVendas((mes-1), index, qtd);
+                    showMenu();
+                    break;
+                case 04:
+                    getAllVendas();
+                    showMenu();
+                    break;
+                case 03:
+                    getAllValores();
+                    showMenu();
+                    break;
+                case 05:
+                    getPrecoMediaAnual();
+                    showMenu();
+                    break;
+                case 07:
+
+                    break;
+            }
+        }catch(Exception ex){
+            System.out.println("Ocorreu um Erro");
+
+        }
+    }
 
     /**
      * Cadastro de produtos
@@ -25,6 +84,7 @@ public class Classe {
                     if (produtos[i] == null) {
                         produtos[i] = nome;
                         valores[i] = valor;
+                        System.out.println("\nProduto Cadastrado com sucesso\n");
                         return true;
                     }
                 }
@@ -46,6 +106,7 @@ public class Classe {
     public boolean setVendas(int mes,int indexProduto,int qtdVendas){
         try{
             vendas[mes][indexProduto] = qtdVendas;
+            System.out.println("\nVenda adicionada com sucesso\n");
             return true;
         }catch(Exception ex) {
             return false;
@@ -57,16 +118,18 @@ public class Classe {
      *
      * @return
      */
-    public String[][] getAll(){
+    public void getAll(){
+        String leftAlignFormat = "| %-15s | %-15s | %-15s |%n";
         String retorno[][] = new String[n][2];
+        System.out.format("+-----------------+-----------------+-----------------+%n");
+        System.out.format("| Index           | Produto         | Valor           |%n");
+        System.out.format("+-----------------+-----------------+-----------------+%n");
         for(int i = 0;i < n;i++){
             if (produtos[i] != null) {
-                System.out.println("[" + produtos[i] + "][" + valores[i] + "]"+getVendas(i));
-                retorno[i][0] = produtos[i];
-                retorno[i][1] = valores[i]+"";
+                System.out.format(leftAlignFormat, i,produtos[i], valores[i]);
+                System.out.format("+-----------------+-----------------+-----------------+%n");
             }
         }
-        return retorno;
     }
 
     /**
@@ -74,16 +137,16 @@ public class Classe {
      * @return
      */
     public void getAllVendas(){
-        String leftAlignFormat = "| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |%n";
+        String leftAlignFormat = "| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |%n";
 
-        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
-        System.out.format("| Produtos        | Valor           | Vendas          | Janeiro         | Fevereiro       | Março           | Abril           | Maio            | Junho           | Julho           | Agosto          | Setembro        | Outubro         | Novembro        | Dezembro        |%n");
-        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
+        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
+        System.out.format("| Produtos        | Vendas          | Janeiro         | Fevereiro       | Março           | Abril           | Maio            | Junho           | Julho           | Agosto          | Setembro        | Outubro         | Novembro        | Dezembro        |%n");
+        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
         for(int i = 0;i < n;i++){
             if (produtos[i] != null) {
                 String[] r = getValorVendas(i);
                 System.out.format(leftAlignFormat,produtos[i], valores[i],r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9],r[10],r[11]);
-                System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
+                System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
             }
         }
     }
@@ -93,16 +156,16 @@ public class Classe {
      */
     public void getAllValores(){
         String retorno[] = new String[n];
-        String leftAlignFormat = "| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |%n";
+        String leftAlignFormat = "| %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |%n";
 
-        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
-        System.out.format("| Produtos        | Valor           | Vendas          | Janeiro         | Fevereiro       | Março           | Abril           | Maio            | Junho           | Julho           | Agosto          | Setembro        | Outubro         | Novembro        | Dezembro        |%n");
-        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
+        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
+        System.out.format("| Produtos        | Valor           | Janeiro         | Fevereiro       | Março           | Abril           | Maio            | Junho           | Julho           | Agosto          | Setembro        | Outubro         | Novembro        | Dezembro        |%n");
+        System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
         for(int i = 0;i < n;i++){
             if (produtos[i] != null) {
                 String[] r = getVendas(i);
                 System.out.format(leftAlignFormat,produtos[i], valores[i],r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9],r[10],r[11]);
-                System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
+                System.out.format("+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+-----------------+%n");
             }
         }
     }
@@ -110,7 +173,7 @@ public class Classe {
     /**
      * Questão 04 e 06
      */
-    public void getPrecoMedioAnual(){
+    public void getPrecoMediaAnual(){
         String retorno[] = new String[n];
         String leftAlignFormat = "| %-15s | %-15s | %-15s |%n";
 
@@ -120,16 +183,54 @@ public class Classe {
         for(int i = 0;i < n;i++){
             if (produtos[i] != null) {
                 double[] r = mediaAnual(getValorVendas(i));
-
-                System.out.format(leftAlignFormat,produtos[i], valores[i],(r[i]/12));
+                System.out.format(leftAlignFormat,produtos[i], valores[i],formatador.format((r[i]/12)));
                 System.out.format("+-----------------+-----------------+-----------------+%n");
             }
         }
     }
 
+
+    /**
+     * Questãao 5
+     */
     public void getMaiorVenda(){
+//        double[] r = new double[n];
+//        for(int i = 0;i < n;i++) {
+//            r[i] = descobrirMaiorValor()
+//        }
+        //int i = descobrirMaiorIndex(mediaAnual(getValorVendas(1)));
+        System.out.println("MaiorProduto:"+descobrirMaiorIndex(mediaAnual(getValorVendas(1))));
+    }
+
+    public int descobrirMaiorIndex(double[] array){
+        // assign first element of array as maximum value
+        double max = array[0];
+        int index = -1;
+
+        for (int i = 1; i < array.length; i++) {
+            if(max > array[i]){
+                index = i-1;
+            }else if(max < array[i]){
+                index = i;
+            }
+
+            max = Math.max(max, array[i]);
+
+        }
+
+        return index;
+    }
+
+    public double descobrirMaiorValor(double[] array){
+        // assign first element of array as maximum value
+        double max = array[0];
 
 
+        for (int i = 1; i < array.length; i++) {
+            max = Math.max(max, array[i]);
+        }
+
+        return max;
     }
 
 
